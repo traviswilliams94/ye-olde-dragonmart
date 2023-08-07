1e5f2f709790cad1e067b7df0531d24a80c8678a
Travis <travis.williams94@gmail.com>
2023-08-06 19:37:13 -0600

    added models and validations

--- server/app.py
+++ server/app.py
@@ -3,13 +3,13 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import Customer, Item, Order, OrderItem
 
 # Views go here!
 
--- server/models.py
+++ server/models.py
@@ -1,6 +1,86 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
 # Models go here!
+
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = 'customers'
+
+    # serialize_rules = 
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    # username = db.Column(db.String, unique=True, nullable=False)
+    # _password_hash = db.Column(db.String)
+    # created_at = db.Column(db.DateTime, server_default=db.func.now())
+    # updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    orders = db.relationship('Order', backref='customer')
+    order_items = association_proxy('orders', 'order_items')
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Customer must have a name')
+        return name
+
+class Item(db.Model, SerializerMixin):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    img_url = db.Column(db.String)
+    description = db.Column(db.String)
+    category = db.Column(db.String, nullable=False)
+    price = db.column(db.Integer, nullable=False)
+
+    orders = db.relationship('OrderItem', backref='item')
+
+    #serilize_rules = 
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if not category:
+            raise ValueError('Category cannot be empty. Must be Weapons, Armor, Tools, or Magic Items.')
+        return category
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if not price and 1 <= price:
+            raise ValueError('Must have a price of 1 or more')
+        return price
+
+class Order(db.Model, SerializerMixin):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.integer, ForeignKey('customers.id'))
+    total = db.Column(db.Float)
+
+    order_items = db.relationship('OrderItem', backref='order')
+    items = association_proxy('order_items', 'item')
+
+
+class OrderItem(db.Model, SerializerMixin):
+    __tablename__ = 'orderitems'
+
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer)
+    order_id = db.Column(db.Integer, ForeignKey('orders.id'))
+    item_id = db.Column(db.Integer, ForeignKey('items.id'))
+
+    @validates('quantity')
+    def validate_quantity(self, key, quantity)
+
+    if quantity > 0:
+        return quantity
+    else:
+        raise ValueError('Quantity must be greater than 0')
+
+    # serialize_rules = 
+
+
