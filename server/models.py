208afb6ea69156924d2a871af5259741eaf3cfbd
Travis <travis.williams94@gmail.com>
2023-08-07 12:35:03 -0600

    added views and requests

--- server/models.py
+++ server/models.py
@@ -1,15 +1,14 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
 
 class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
-
-    # serialize_rules = 
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
@@ -17,18 +16,34 @@ class Customer(db.Model, SerializerMixin):
     admin = db.Column(db.Boolean)
 
     username = db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String)
+    _password_hash = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     orders = db.relationship('Order', backref='customer')
     order_items = association_proxy('orders', 'order_items')
 
+    serialize_rules = ('-_password_hash', '-orders', '-created_at', '-updated_at')
+
     @validates('name')
     def validate_name(self, key, name):
         if not name:
             raise ValueError('Customer must have a name')
         return name
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
 
 class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
@@ -42,12 +57,12 @@ class Item(db.Model, SerializerMixin):
 
     orders = db.relationship('OrderItem', backref='item')
 
-    #serilize_rules = 
+    serilize_rules = ('-order_items', '-created_at', '-updated_at')
 
     @validates('category')
     def validate_category(self, key, category):
         if not category:
-            raise ValueError('Category cannot be empty. Must be Weapons, Armor, Tools, or Magic Items.')
+            raise ValueError('Category cannot be empty. Must be weapons, armor, tools, or magic items.')
         return category
 
     @validates('price')
@@ -68,6 +83,8 @@ class Order(db.Model, SerializerMixin):
     order_items = db.relationship('OrderItem', backref='order')
     items = association_proxy('order_items', 'item')
 
+    serialize_rules = ('-order_items', '-customer_id', 'updated_at')
+
 
 class OrderItem(db.Model, SerializerMixin):
     __tablename__ = 'orderitems'
@@ -79,6 +96,8 @@ class OrderItem(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    serialize_rules = ('-created_at', '-updated_at', 'order', '-order_id', '-item_id')
+
     @validates('quantity')
     def validate_quantity(self, key, quantity)
 
@@ -87,6 +106,6 @@ class OrderItem(db.Model, SerializerMixin):
     else:
         raise ValueError('Quantity must be greater than 0')
 
-    # serialize_rules = 
+   
 
 
